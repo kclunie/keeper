5b6d648de158bd25d5f0ff453305d879ce74014c
Kristen Clunie <kcluniesa@yahoo.com>
2021-07-02 10:55:57 -0400

    add footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header.js";
 import Footer from "./Footer.js";
+import Note from "./Note.js"
 
 function App(){
     return <div>
     <Header />
+    <Note />
     <Footer />
 </div>
 }
