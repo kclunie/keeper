332ca53f67d82b5be34370737b4da41d5eb751f0
Kristen Clunie <kcluniesa@yahoo.com>
2021-07-10 12:46:13 -0400

    add form

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea(){
+
+    const [note, setNote] = useState({
+         title: "",
+         content: ""   
+    });
+
+    function handleChange(event){
+      const {name, value} = event.target;
+      setNote(prevNote => {
+          return {
+              ...prevNote, [name]: value
+          }
+      })
+    }
+
+    function submitNote(){
+        
+    }
+
     return (
         <div>
-        <form>
-        <input name="title" placeholder="Title"/>
-        <textarea name="content" placeholder="add a note..." rows="3"/>
-        <button>Add</button>
-        </form>
+            <form>
+                <input name="title" value={note.title} onChange={handleChange} placeholder="Title"/>
+                <textarea name="content" value={note.content} onChange={handleChange} placeholder="add a note..." rows="3"/>
+                <button onClick={submitNote}>Add</button>
+            </form>
         </div>
     )
 }
